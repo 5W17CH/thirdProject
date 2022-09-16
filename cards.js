4c569c8fc14e99b79a3b355fab4f09d67bb4e067
Hissue556 <hissue5.56@gmail.com>
2022-09-16 20:25:45 +0200

    form validation complete

--- cards.js
+++ cards.js
@@ -11,30 +11,54 @@ const dateMonth = document.getElementById("datemonth");
 const dateYear = document.getElementById("dateyear");
 const cvcInput = document.getElementById("cvcinput");
 
-var re = new RegExp (/^[a-zA-Z]+$/);
+var re = new RegExp ("^[0-9]+$");
+var re2 = new RegExp ("^[a-zA-Z ]+$");
+
+
 
 inputName.addEventListener("input", (e) => {
     cardName.innerHTML = e.target.value.toUpperCase();
+    if(re2.test(inputName.value)) {
+        document.getElementById("errorname").style.visibility = "hidden";
+    } else {
+        document.getElementById("errorname").style.visibility = "visible";
+    }
 });
 
 inputNumber.addEventListener("input", (e) => {
-    cardNumber.innerHTML = e.target.value.replace(/(.{4})/g, '$1 ');
+    cardNumber.innerHTML = e.target.value.replace(/[^\dA-Z]/g, '').replace(/(.{4})/g, '$1 ');
     if(re.test(inputNumber.value)) {
-        document.getElementById("popi").style.visibility = "visible";
+        document.getElementById("errornum").style.visibility = "hidden";
     } else {
-        document.getElementById("popi").style.visibility = "hidden";
+        document.getElementById("errornum").style.visibility = "visible";
     }
 });
 
 dateMonth.addEventListener("input", (e) => {
     month.innerHTML = e.target.value.replace(/[^\dA-Z]/g, '');
+    if(re.test(dateMonth.value)) {
+        document.getElementById("errordate").style.visibility = "hidden";
+    } else {
+        document.getElementById("errordate").style.visibility = "visible";
+    }
 })
 
 dateYear.addEventListener("input", (e) => {
     year.innerHTML = e.target.value.replace(/[^\dA-Z]/g, '');
+    if(re.test(dateYear.value)) {
+        document.getElementById("errordate").style.visibility = "hidden";
+    } else {
+        document.getElementById("errordate").style.visibility = "visible";
+    }
 })
 
 cvcInput.addEventListener("input", (e) => {
     CVC.innerHTML = e.target.value.replace(/[^\dA-Z]/g, '');
+    if(re.test(cvcInput.value)) {
+        document.getElementById("errorcvc").style.visibility = "hidden";
+    } else {
+        document.getElementById("errorcvc").style.visibility = "visible";
+    }
 })
 
+//when button is pressed check all fields filled and (&&) no errors if true execute function and transform container to "thank you we've added your card details"//
